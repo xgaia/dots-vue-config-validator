f6789ef4a368d4bb5bcfd57fbde64439a6ed5eca
xgaia <xgaia@gmx.com>
2026-03-04 17:59:28 +0100

    feat: add entrypoint for consumer project

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// Entry point for the dots-vue-config-validator package
+// Export the Zod schema so consumer projects can reuse it.
+import { DotsConfig, GenericConfig } from "./schema.js";
+
+export { DotsConfig, GenericConfig };
